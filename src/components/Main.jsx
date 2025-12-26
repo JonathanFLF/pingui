7be4abe6141ed3ab0c595fcfe3b83d6d5f8d0294
JonathanFLF <ferney5585@gmail.com>
2025-12-26 00:48:48 -0500

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,7 +13,7 @@ export function Main () {
                     <h1 className='font-sans font-bold text-gray-400 text-2xl'> 
                     Tu presencia hace mis días mejores, y eso para mí lo dice todo </h1>
                     <h1 className='font-sans font-bold text-gray-400 text-2xl'> 
-                   ! Te quiero ¡ </h1>
+                   ¡Te quiero! </h1>
                     <img src={image} className='w-72'/>
 
                     <p className='font-mono text-yellow-100'> {textP} </p>
